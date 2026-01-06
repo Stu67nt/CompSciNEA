a8433001ede602ba09c6e9169136c6e83c1d717c
Tanzil Chowdhury <tanzilchowdhury2014@gmail.com>
2026-01-06 14:35:50 +0000

    .

--- Components.py
+++ Components.py
@@ -1,6 +1,7 @@
 from widgets import *
 import customtkinter
 from customtkinter import CTkFrame
+import tkinter as tk
 
 class ToDoList(customtkinter.CTkFrame):
     """
@@ -27,7 +28,7 @@ class ToDoList(customtkinter.CTkFrame):
         self.input.grid(row=2, column=0, padx=10, pady=(10, 10), sticky="nwe")
 
         self.buttons = ButtonFrame(self, [["Create Task", self.balls], ["Delete Tasks", self.balls]], is_horizontal = True)
-        self.buttons.grid(row=3, column=0, padx=10, pady=(10, 10), sticky="nw")
+        self.buttons.grid(row=3, column=0, padx=10, pady=(10, 10), sticky="nwe")
 
     def balls(self):
         print("Balls")
@@ -47,17 +48,34 @@ class TimerCreate(customtkinter.CTkFrame):
         self.title_label = customtkinter.CTkLabel(self, text="Timer", fg_color="gray30", corner_radius=6)
         self.title_label.grid(row=0, column=0, padx=10, pady=(10, 10), sticky="new", columnspan=2)
 
+
         self.hours_label = customtkinter.CTkLabel(self, text="Enter Hours: ", fg_color="transparent", corner_radius=6)
         self.hours_label.grid(row=1, column=0, padx=10, pady=(10, 10), sticky="nw")
+
+        self.hours_spinbox = tk.Spinbox(self, from_=0, to=23, repeatdelay=500, repeatinterval=100,
+                     font=("Arial", 20))
+        self.hours_spinbox.grid(row=1, column=1, padx=10, pady=(10, 10), sticky="ne")
+
 
         self.mins_label = customtkinter.CTkLabel(self, text="Enter Miniutes: ", fg_color="transparent", corner_radius=6)
         self.mins_label.grid(row=2, column=0, padx=10, pady=(10, 10), sticky="nw")
 
+        self.mins_spinbox = tk.Spinbox(self, from_=0, to=59, repeatdelay=500, repeatinterval=100,
+                     font=("Arial", 20))
+        self.mins_spinbox.grid(row=2, column=1, padx=10, pady=(10, 10), sticky="ne")
+
+
         self.secs_label = customtkinter.CTkLabel(self, text="Enter Seconds: ", fg_color="transparent", corner_radius=6)
         self.secs_label.grid(row=3, column=0, padx=10, pady=(10, 10), sticky="nw")
 
+        self.secs_spinbox = tk.Spinbox(self, from_=0, to=59, repeatdelay=500, repeatinterval=100,
+                     font=("Arial", 20))
+        self.secs_spinbox.grid(row=3, column=1, padx=10, pady=(10, 10), sticky="ne")
+
+
         self.button = ButtonFrame(self, self.BUTTON, is_horizontal = True, button_sticky = "nw")
-        self.button.grid(row=4, column=0, padx=10, pady=(10, 10), sticky="nw")
+        self.button.grid(row=4, column=0, padx=10, pady=(10, 10), sticky="nsw")
 
     def test(self):
         print("test")
+
--- main.py
+++ main.py
@@ -1,36 +1,72 @@
 import customtkinter
+import tkinter as tk
 from widgets import *
 from Components import *
 
-class App(customtkinter.CTk): # Inheriting CTk class
-    def __init__(self, title: str="My App"):
+
+class Home(customtkinter.CTk): # Inheriting CTk class
+    def __init__(self, master):
         super().__init__() # Calls parent class
 
-        # Initialising Window
-        self.title(title)
-        self.grid_rowconfigure((0,4), weight=1)
-        self.grid_columnconfigure((0,1), weight=1)
-        customtkinter.set_appearance_mode("dark") # light/dark/system (system is not functional on linux)
-
         # Creating Home Bar
-        BUTTONS = [["Home", self.hi], ["Tracks", self.hi], ["Playlists", self.hi], ["Music Finder", self.hi],
+        HOMEBAR = [["Home", self.hi], ["Tracks", self.hi], ["Playlists", self.hi], ["Music Finder", self.hi],
                    ["Favourites", self.hi], ["Settings", self.hi]]
 
-        self.buttons = ButtonFrame(self, button_values = BUTTONS, is_horizontal = True)
-        self.buttons.grid(row=0, column=0, padx=10, pady=(10,10), sticky="new", columnspan = 2)
+        BUTTONS = [["Scan Music", self.hi], ["Add Folder", self.hi]]
 
         # Creating To Do List
-        self.todo = ToDoList(self)
-        self.todo.grid(row=1, column=0, padx=(10,10), pady=(10,10), sticky="nwe")
+        self.todo = ToDoList(master)
+        self.todo.grid(row=1, column=0, padx=(10,10), pady=(10,10), sticky="nwe", rowspan = 2)
 
         # Creating timer
-        self.timer = TimerCreate(self)
+        self.timer = TimerCreate(master)
         self.timer.grid(row=1, column=1, padx=(10,10), pady=(10,10), sticky = "nwse")
+
+        self.buttons = ButtonFrame(master, button_values=BUTTONS, is_horizontal=True)
+        self.buttons.grid(row=2, column=1, padx=10, pady=(10, 10), sticky="new")
 
     def hi(self):
         print("hi")
 
+class Tracks(customtkinter.CTk): # Inheriting CTk class
+    def __init__(self, master, title: str="My App"):
+        super().__init__() # Calls parent class
+
+        TOPBAR = [["Select Multiple", self.hi]]
+        song_count = 50
+
+        self.topbar = ButtonFrame(master, button_values=TOPBAR, is_horizontal=True, title= f"{song_count} Songs")
+        self.topbar.grid(row=1, column=0, padx=10, pady=(10, 10), sticky="new")
+
+    def hi(self):
+        print("hi")
+
+class MyTabView(customtkinter.CTkTabview):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+		# create tabs
+        self.add("Home")
+        self.add("Tracks")
+
+		# add widgets on tabs
+        self.home = Home(master=self.tab("Home"))
+        self.home.grid()
+
+        self.tracks = Tracks(master=self.tab("Tracks"))
+        self.tracks.grid()
+
+class App(customtkinter.CTk):
+    def __init__(self, title="My App"):
+        super().__init__()
+
+        # Initialising Window
+        self.title(title)
+        customtkinter.set_appearance_mode("dark")  # light/dark/system (system is not functional on linux)
+
+        self.tab_view = MyTabView(master=self)
+        self.tab_view.grid(row=0, column=0, padx=20, pady=20)
 
 
-app = App("Balls") # Creating app object
-app.mainloop() # Needed to actually display the app.
+app = App("Balls")
+app.mainloop()
